55345c28f627a580fb5ee31011265e16af3a21df
Arash Kadkhodaei <slasharash@protonmail.com>
2020-05-21 16:54:18 +0430

    add size to Emoji component

--- packages/pwa/src/components/Emoji/Emoji.tsx
+++ packages/pwa/src/components/Emoji/Emoji.tsx
@@ -1,18 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
 
 type Props = {
   label?: string
+  size?: 'large' | 'medium' | 'small'
   symbol: string
 }
 
-const Emoji: React.FC<Props> = ({ label, symbol }) => (
-  <span
+type ContentProps = {
+  size: 'large' | 'medium' | 'small'
+}
+
+const sizeMap = {
+  large: '3rem',
+  medium: '2rem',
+  small: '1rem',
+}
+
+const Content = styled('span')<ContentProps>`
+  font-size: ${(props) => sizeMap[props.size]};
+`
+
+const Emoji: React.FC<Props> = ({ label, size = 'medium', symbol }) => (
+  <Content
     role="img"
     aria-label={label ? label : ''}
     aria-hidden={label ? 'false' : 'true'} // if aria-hidden is true it will be ignored by the screen reader
+    size={size}
   >
     {symbol}
-  </span>
+  </Content>
 )
 
 export default Emoji
